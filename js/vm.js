dcc5c5ca2f79339b39c5577fc6d1fe9135716e8d
Tanawit <tanawit@intermedisoft.com>
2018-10-16 16:03:38 +0700

    update

--- js/vm.js
+++ js/vm.js
@@ -1,8 +1,7 @@
 var FIX = {},
-    listBaseDepartment = [],
-    customForm = document.getElementById('ifrmCustomForm').contentWindow;
+    listBaseDepartment = [];
 
-Vue.component('v-select', VueSelect.VueSelect);
+Vue.component('vue-multiselect', window.VueMultiselect.default)
 
 var vm = new Vue({
     el: '#vm',
@@ -15,12 +14,13 @@ var vm = new Vue({
         baseCustomTemplate: {},
         classInActive: 'fa fa-eye-slash fa-2x',
         isHaveDepartmentTemplate: false,
-        listSelectDepartment: [],
+        show: true,
     },
     beforeCreate: async function () {
         await init();
         this.FIX = FIX;
-        this.listFixCustomTemplateGroup = listFixCustomTemplateGroup;
+        let _listFixCustomTemplateGroup = await useFetch({ url: '/imed/ws/masterdatas/bases/customtemplate' });
+        this.listFixCustomTemplateGroup = _listFixCustomTemplateGroup.RESULT_DATA;
         let _listBaseDepartment = await useFetch({ url: '/imed/ws/masterdatas/bases/department'});
         this.listBaseDepartment = _listBaseDepartment.RESULT_DATA;
     },
@@ -38,25 +38,37 @@ var vm = new Vue({
             if (keyword) {
                 let result = await useFetch({ url: `/imed/ws/setups/custom_forms?custom_template_group=${keyword}`});
                 this.listBaseCustomTemplate = result.slice(0);
-                console.log('update list');
             }
         },
         readEditTemplate: function (el) {
+            this.$refs.notification.work();
             this.baseCustomTemplate = Object.assign({}, el);
 
-            if (this.baseCustomTemplate.listBaseCustomTemplateDepartment.length) {
-                let listBaseDepartment = this.baseCustomTemplate.listBaseCustomTemplateDepartment;
-                let result = listBaseDepartment.map(el => el.baseDepartmentId);
-                $('#ddl_selectDepartment').val(result);
-                $('#ddl_selectDepartment').trigger('change');
+            // ถ้า template มี listBaseDepartment จะต้องนำค่าไปเพิ่มใน ComboBox ก่อน
+            if (this.baseCustomTemplate.listBaseCustomTemplateDepartment && this.baseCustomTemplate.listBaseCustomTemplateDepartment.length) {
+                let listTemplateDepartment = this.baseCustomTemplate.listBaseCustomTemplateDepartment;
+                let _temp = listTemplateDepartment.map(templateDepartment => {
+                    let result = this.listBaseDepartment.find(baseDepartment => baseDepartment.objectID === templateDepartment.baseDepartmentId);
+                    result.baseCustomTemplateId = templateDepartment.baseCustomTemplateId;
+                    result.templateDepartmentID = templateDepartment.objectID;
+                    return result;
+                });
+
+                this.selectDepartment = _temp.slice(0);
+            } else {
+                this.selectDepartment = [];
             }
+            let customForm = document.getElementById('ifrmCustomForm').contentWindow;
+            customForm.clear();
+            customForm.importCustomForm(this.baseCustomTemplate.listBaseCustomForm);
 
             this.changeTemplateGroup();
             this.setClassInActive();
+            this.$refs.notification.clear();
         },
         changeTemplateGroup: function () {
             let template = this.listFixCustomTemplateGroup.find(el => el.objectID === this.baseCustomTemplate.fixCustomTemplateGroup);
-            this.isHaveDepartmentTemplate = template.is_department_template === FIX.FixBooleanStatus.TRUE;
+            this.isHaveDepartmentTemplate = template.isDepartmentTemplate === FIX.FixBooleanStatus.TRUE;
         },
         toggleInActive: function () {
             let url, status;
@@ -79,67 +91,79 @@ var vm = new Vue({
                 : "fa fa-eye-slash fa-2x";
         },
         craeteNewTemplate: function () {
-            this.baseCustomTemplate = { active: this.FIX.FixBooleanStatus.TRUE };
+            this.baseCustomTemplate = {
+                active: this.FIX.FixBooleanStatus.TRUE,
+            };
+            document.getElementById('ifrmCustomForm').contentWindow.clear();
             this.setClassInActive();
         },
         saveTemplate: async function () {
-            if (!this.baseCustomTemplate) {
-                alert('กรุณากรอกชื่อ Template');
+            if (!this.baseCustomTemplate.description) {
+                this.$refs.notification.trigger({ type: 'warning', message: 'กรุณากรอกชื่อ Template' });
                 return;
             }
 
             // ถ้า template นี้เลือก department ได้
             if (this.isHaveDepartmentTemplate) {
-                let listDepartment = $('#ddl_selectDepartment').val(),
-                    _temp = [],
-                    listBaseCustomTemplateDepartment = this.baseCustomTemplate.listBaseCustomTemplateDepartment;
+                let _temp = [];
 
-                listDepartment.forEach(departmentID => {
-                    let oldData;
-                    if (listBaseCustomTemplateDepartment)
-                        oldData = listBaseCustomTemplateDepartment.filter(templateDepartment => templateDepartment.baseDepartmentId === departmentID);
-
-                    if (oldData) {
-                        _temp.push(oldData);
+                this.selectDepartment.forEach(el => {
+                    if ("templateDepartmentID" in el) {
+                        _temp.push({
+                            objectID: el.templateDepartmentID,
+                            baseCustomTemplateId: el.baseCustomTemplateId,
+                            baseDepartmentId: el.objectID
+                        });
                     } else {
-                        _temp.push({ baseDepartmentId: departmentID });
+                        _temp.push({ baseDepartmentId: el.objectID });
                     }
                 });
 
                 this.baseCustomTemplate.listBaseCustomTemplateDepartment = [..._temp];
             }
 
-            let param = JSON.stringify(this.baseCustomTemplate);
+            // ดึงข้อมูลจาก custom form
+            let customForm = document.getElementById('ifrmCustomForm').contentWindow;
+            this.baseCustomTemplate.listBaseCustomForm = customForm.exportCustomForm();
+
+            let clone = Object.assign({}, this.baseCustomTemplate),
+                responseResult;
 
             if ("objectID" in this.baseCustomTemplate) {
-                await useFetch({
+                /** UPDATE */
+                responseResult = await useFetch({
                     url: `/imed/ws/setups/custom_forms/${this.baseCustomTemplate.objectID}`,
                     method: 'PUT',
-                    body: param
+                    body: clone
                 });
             } else {
-                await useFetch({
+                /** INSERT */
+                responseResult = await useFetch({
                     url: '/imed/ws/setups/custom_forms',
-                    body: param,
-                    method: 'POST'
+                    method: 'POST',
+                    body: clone
                 });
+
+                responseResult = await useFetch({url: `/imed/ws/setups/custom_forms/${responseResult.objectID}`});
             }
 
             this.fillterListTemplate();
+            this.readEditTemplate(responseResult);
+            this.$refs.notification.success();
         },
         deleteTemplate: async function () {
             if ("objectID" in this.baseCustomTemplate) {
                 if (confirm('ยืนยันที่จะลบ Template?')) {
-                    debugger;
                     await useFetch({
                         url: `/imed/ws/setups/custom_forms/${this.baseCustomTemplate.objectID}`,
                         body: JSON.stringify(this.baseCustomTemplate),
                         method: 'DELETE'
                     });
 
-                    $('#ddl_selectDepartment').val(null).trigger('change');
                     this.craeteNewTemplate();
                     this.fillterListTemplate();
+                    this.selectDepartment = [];
+                    document.getElementById('ifrmCustomForm').contentWindow.clear();
                 }
             }
         },
@@ -173,23 +197,39 @@ async function init() {
 };
 
 async function useFetch({ url, method = 'GET', body = ''}) {
-    if (method.toUpperCase() === "GET") {
+    try {
         let response;
-        try {
-            response =  await fetch(url);
-        } catch(err) {
-            console.error(err);
+
+        if (!!body) {
+            body = ((typeof body).toLowerCase() === String.name.toLowerCase())
+                ? body
+                : JSON.stringify(body);
         }
 
-        return await response.json();
-    } else {
-        await fetch(url, {
-            headers: { "Content-Type": "application/json;"},
-            method,
-            body,
-        }).then(res => {
-            if(res.ok)
-                console.log('fetch complete!');
-        }).catch(err => console.error(err));
+        switch (method.toUpperCase()) {
+            case "GET":
+                response =  await fetch(url);
+                return await response.json();
+            case "POST":
+            case "PUT":
+                response = await fetch(url, {
+                    headers: { "Content-Type": "application/json;"},
+                    method,
+                    body,
+                });
+                return await response.json();
+            case "DELETE":
+                await fetch(url, {
+                    headers: { "Content-Type": "application/json;"},
+                    method,
+                    body,
+                });
+                break;
+            default:
+                break;
+        }
+    } catch(err) {
+        vm.$refs.notification.error();
+        console.error(err);
     }
 }
